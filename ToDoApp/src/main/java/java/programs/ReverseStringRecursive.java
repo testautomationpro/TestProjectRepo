e821880e7a6c768fedf0dfead6fb367e39797aed
honey <honey@10.0.0.6>
2019-08-10 19:14:40 -0700

    created java pgm to reverse string

---
+++ ToDoApp/src/main/java/java/programs/ReverseStringRecursive.java
@@ -0,0 +1,21 @@
+package java.programs;
+
+public class ReverseStringRecursive {
+	
+	public static void main(String []str) {
+		String s="MyJava";
+	
+		System.out.println(recursiveReverse(s));
+	}
+		static String recursiveReverse(String s)
+		{
+			if((null==s) || (s .length() <= 1))
+			{
+				return s;
+			}
+				return recursiveReverse(s.substring(1)) + s.charAt(0);
+						
+		
+	}
+
+}
